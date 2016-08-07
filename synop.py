b1d6489d32599ee83bb51a7ce810c24ec0d3a7dd
nebulon42 <nebulon42@yandex.com>
2016-08-07 19:25:25 +0200

    first commit: support basic decoding of synop bulletins and filtering by country and station ids

---
+++ synop.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import argparse
+import re
+import sys
+import yaml
+
+stationList = []
+countryList = None
+# use NOAA bulletin separator line to split up bulletins
+bulletinSeparator = '####[0-9]{9}####'
+
+args = None
+
+def main():
+    global args
+    global countryList
+    global stationList
+
+    parser = argparse.ArgumentParser(description='Parses NOAA TAC SYNOP bulletins.')
+    parser.add_argument('input',
+                        metavar='input-file',
+                        help='the input file with SYNOP bulletins')
+    parser.add_argument('-v', '--verbose', dest='verbose',
+                        help='print verbose output of filtering etc.',
+                        action='store_true')
+    parser.add_argument('-f', '--filter', dest='filterfile',
+                        metavar='filter-file',
+                        help='YAML file specifying country and station filters',
+                        required=False,
+                        default=None)
+    args = parser.parse_args()
+
+    if args.filterfile:
+        filterSpec = None
+        try:
+            filterFile = open(args.filterfile, 'r')
+            filterSpec = yaml.safe_load(filterFile.read())
+            filterFile.close()
+        except IOError:
+            sys.exit('Could not read filter file, please check if it exists. Exiting.')
+
+        if 'countries' in filterSpec:
+            countryList = ''
+            for country in filterSpec['countries']:
+                countryList += country + '|'
+            countryList = countryList[:len(countryList) - 1]
+        else:
+            countryList = '[A-Z]{2}'
+
+        if 'stations' in filterSpec and 'synop' in filterSpec['stations']:
+            stationList = filterSpec['synop']['stations']
+    else:
+        countryList = '[A-Z]{2}'
+
+    data = ''
+    try:
+        inputFile = open(args.input, 'r')
+        data = inputFile.read()
+        inputFile.close()
+        # convert newlines into spaces and remove carriage returns
+        data = data.replace('\n', ' ')
+        data = data.replace('\r', '')
+        # collapse spaces
+        data = ' '.join(data.split())
+    except IOError:
+        sys.exit('Could not read input file, please check if it exists. Exiting.')
+
+    # specify regular expression of what separates bulletins in the text file
+    bulletins = re.split(bulletinSeparator, data)
+
+    count = 0
+    for bulletin in bulletins:
+        bulletin = bulletin.strip()
+        # first one will be usually empty
+        if len(bulletin) == 0:
+            continue
+        count += 1
+        processBulletin(bulletin, count)
+
+def processBulletin(bulletin, count):
+    mixedBulletin = False
+
+    # examine bulletin header
+    # it is of the form TTAAii CCCC YYGGgg (BBB)
+    # TT = data type, AA = country or region, ii = sequence nr
+    # CCCC = issuer, YYGGgg = day of month, hour UTC and minute UTC
+    # BBB is either RRx, CCx or AAx, where x = A..Z and RR = additional info (new data), CC = correction, AA = amendment
+    # for SYNOP we are interested only in SI..., SM.... and SN....
+    # see https://www.wmo.int/pages/prog/www/ois/Operational_Information/Publications/WMO_386/AHLsymbols/TableB1.html
+    # countries are two letter character codes (non-ISO), can be filtered
+    bulletinHead = re.match('^S[IMN](' + countryList + ')[0-9]{2}\s([A-Z]{4})', bulletin)
+    if bulletinHead:
+        verbosePrint('bulletin ' + str(count) + ', country: ' + bulletinHead.group(1) + ', issuer: ' + bulletinHead.group(2))
+        # consume first part of bulletin incl. CCCC and YYGGgg
+        bulletin = bulletin[19:]
+        # consume optional BBB modifier
+        bulletinMod = re.match('^(AA|CC|RR)([A-Z])\s', bulletin)
+        if bulletinMod:
+            print('modifier: ' + bulletinMod.group(1) + ', sequence: ' + bulletinMod.group(2))
+            bulletin = bulletin[4:]
+
+        # decide whether MMMM group (e.g. AAXX) occurs only once or more often
+        # since we are only interested in AAXX here, we can safely assume that if XX occurs only once it is only present at the start of the bulletin
+        if bulletin.count('XX') <= 1:
+            if not bulletin.startswith('AAXX'):
+                verbosePrint('discarding bulletin not containing data from fixed surface land stations.')
+                return
+            # consume MMMM and YYGGi group which is valid for the entire bulletin
+            bulletin = bulletin[4:]
+            windIndicator = int(bulletin[5:6])
+            print('day: ' + bulletin[1:3] + ', hour: ' + bulletin[3:5])
+            bulletin = bulletin[7:]
+        else:
+            mixedBulletin = True
+
+        # we have now reached the level of individual stations
+        stations = bulletin.split('=')
+        for station in stations:
+            station = station.strip()
+            # discard empty report
+            if len(station) == 0:
+                continue
+            if station.endswith('NIL'):
+                verbosePrint('discarding NIL report.')
+                continue
+
+            if mixedBulletin:
+                if not station.startswith('AAXX'):
+                    verbosePrint('discarding station report not containing data from fixed surface land stations.')
+                    continue
+                else:
+                    # consume MMMM and YYGGi group of station
+                    station = station[4:]
+                    windIndicator =  int(station[5:6])
+                    print('day: ' + station[1:3] + ', hour: ' + station[3:5])
+                    station = station[7:]
+            # consume IIiii (station number)
+            stationId = station[:5]
+            if not stationList or int(stationId) in stationList:
+                station = station[6:]
+                print('decoding report from station ' + stationId + '.')
+                print(station)
+            else:
+                verbosePrint('discarding report from station ' + stationId + ', not in list.')
+    else:
+        verbosePrint('discarding non-SYNOP or geographically irrelevant bulletin.')
+
+def verbosePrint(output):
+    if args and args.verbose:
+        print(output)
+
+if __name__ == "__main__":
+    main()
